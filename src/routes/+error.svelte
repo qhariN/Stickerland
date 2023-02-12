2bddb9dae1f733868f7ca8a4ec8a366c9b4b495f
paul351 <38229848+paul351@users.noreply.github.com>
2023-02-12 11:59:20 -0500

    intit project

---
+++ src/routes/+error.svelte
@@ -0,0 +1,1 @@
+<h1>Welcome to 404 ERROR</h1>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let counter = 0;
+    let number_to_sum: number;
+
+    function sumCounter (number: number = 1) {
+        counter += number;
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<input type="number" placeholder="Numero a sumar" bind:value={number_to_sum}>
+<button on:click={ () => {sumCounter(number_to_sum) }}>btn {counter}</button> <br>
+{number_to_sum}
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
