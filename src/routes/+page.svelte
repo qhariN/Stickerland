cf23b001e8fedbe400b8cd1f8a232d762b8ef615
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-03-04 20:24:53 -0500

    add styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 
   let root: HTMLInputElement
   let instance: FilePond.FilePond
+  let publicId: string | undefined
 
   afterUpdate(async () => {
     await import('@lottiefiles/lottie-player')
@@ -25,7 +26,8 @@
           },
           onload: (response) => {
             const data = JSON.parse(response)
-            return data.secure_url
+            publicId = data.public_id.replace('stickerland/', '')
+            return data.public_id
           }
         }
       },
@@ -36,8 +38,8 @@
       instantUpload: false,
       allowProcess: false,
       maxFileSize: '10MB',
-      imagePreviewHeight: 304,
-      acceptedFileTypes: ['image/png', 'image/jpeg', 'image/gif', 'image/svg+xml', 'image/webp'],
+      imagePreviewHeight: 280,
+      acceptedFileTypes: ['image/png', 'image/jpeg', 'image/gif', 'image/webp'],
       labelIdle: 'Arrastra y suelta tu imagen o <br><span class="filepond--label-action"> Busca en tu computadora </span>',
       labelMaxFileSizeExceeded: 'El archivo es muy grande',
       labelMaxFileSize: 'Tamaño máximo del archivo es {filesize}',
@@ -52,42 +54,50 @@
   })
 
   const handleFileUpload = () => {
-    instance.processFile().then((file) => {
-      console.log(file)
-    })
+    instance.processFile()
   }
 </script>
 
-<lottie-player autoplay loop src="bg.json" speed=".3" intermission="0" class="left-0 right-0 ml-auto mr-auto absolute w-2/3 2xl:w-1/2 -z-10 opacity-50"></lottie-player>
-<div class="min-h-screen flex flex-col">
-  <main class="max-w-[400px] mx-auto w-full flex-grow flex items-center">
+<div class="min-h-screen flex flex-col relative overflow-hidden">
+  <lottie-player autoplay loop src="bg.json" speed=".3" intermission="0" class="right-24 sm:left-0 sm:right-0 top-0 bottom-0 ml-auto mr-auto mt-0 mb-0 absolute w-[200%] sm:w-full lg:w-11/12 2xl:w-1/2 -z-10 opacity-50"></lottie-player>
+  <main class="max-w-[440px] mx-auto w-full flex-grow flex items-center p-2 sm:p-8">
     <div class="card w-full space-y-8">
       <div class="space-y-3">
         <h1 class="text-center text-5xl font-bold">Stickerland</h1>
         <p class="description">
-          Crea y comparte tus propios<br />
-          stickers para WhatsApp
+          Crea y comparte tus propios stickers para WhatsApp
         </p>
       </div>
       <div class="filepond--wrapper">
         <input bind:this={root} type="file" name="file" />
       </div>
-      <button on:click={handleFileUpload} class="w-full">
-        Generate
-      </button>
+      <div class="flex flex-col items-center gap-2">
+        {#if !publicId}
+          <button on:click={handleFileUpload} class="w-full">
+            Generar
+          </button>
+        {:else}
+          <a href="https://wa.me/51935858346?text=sticker-ID%20{publicId}" target="_blank" rel="noopener noreferrer">
+            Compartir
+          </a>
+        {/if}
+        <p>
+          o ver galería de stickers
+        </p>
+      </div>
     </div>
   </main>
 </div>
 
 <style lang="postcss">
   .card {
-    @apply bg-white/60 rounded-3xl p-8 shadow-xl shadow-black/5;
+    @apply bg-white/60 rounded-3xl px-8 py-10 shadow-xl shadow-black/5;
   }
-  button {
+  button, a {
     @apply rounded-2xl px-5 backdrop-blur-md text-white text-xl font-bold;
     @apply transition-colors duration-200;
   }
-  button {
+  button, a {
     @apply py-3 flex items-center justify-center;
     @apply bg-[#DB91DC] hover:bg-[#E09FE0] active:bg-[#D98ED9];
     @apply disabled:cursor-not-allowed disabled:bg-[#DB91DC]/50;
