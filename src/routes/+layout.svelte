ad955e93c803ac0e04301c0ff3d3d7e16248a8f2
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-03-05 22:50:39 -0500

    add meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,21 @@
 
 <svelte:head>
   <title>Stickerland</title>
+  <link rel="canonical" href="https://stickerland.vercel.app/" />
+  <meta name="title" content="Stickerland" />
+  <meta name="description" content="Crea y comparte tus propios stickers para WhatsApp" />
+  <meta name="author" content="Jhorman Ruswel Tito Tito" />
+  <meta name="robots" content="index,follow" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Stickerland" />
+  <meta property="og:site_name" content="Stickerland" />
+  <meta property="og:description" content="Crea y comparte tus propios stickers para WhatsApp" />
+  <meta property="og:url" content="https://stickerland.vercel.app/" />
+  <meta property="og:locale" content="es" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:url" content="https://stickerland.vercel.app/" />
+  <meta name="twitter:title" content="Stickerland" />
+  <meta name="twitter:description" content="Crea y comparte tus propios stickers para WhatsApp" />
 </svelte:head>
 
 <lottie-player autoplay loop src="bg.json" speed=".3" intermission="0" class="absolute right-24 sm:left-0 sm:right-0 top-0 bottom-0 ml-auto mr-auto mt-0 mb-0 w-[200%] sm:w-full lg:w-11/12 2xl:w-1/2 -z-10 opacity-50"></lottie-player>
--- src/routes/galery/+page.svelte
+++ src/routes/galery/+page.svelte
@@ -15,7 +15,7 @@
   {:then assets}
     <div class="grid grid-cols-2 gap-4">
       {#each assets as asset}
-        <a transition:slide class="rounded-2xl bg-white/50 p-3 transition-transform hover:scale-105 hover:rotate-3 cursor-pointer shadow-sm" href="https://wa.me/51935858346?text=sticker-ID%20{asset.public_id}" target="_blank" rel="noopener noreferrer">
+        <a class="rounded-2xl bg-white/50 p-3 transition-transform hover:scale-105 hover:rotate-3 cursor-pointer shadow-sm" href="https://wa.me/51935858346?text=sticker-ID%20{asset.public_id}" target="_blank" rel="noopener noreferrer">
           <img src={asset.url} alt="sticker" class="rounded-xl" />
         </a>
       {/each}
